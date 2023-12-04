f2158ad01f5a5bc8a21574dd27b89b3995765a26
Chia-Lun Wu <chia@flagright.com>
2023-12-05 00:15:55 +0100

    update

--- src/App.js
+++ src/App.js
@@ -66,8 +66,10 @@ function RatioTool() {
           <InputGroup.Text>%</InputGroup.Text>
         </InputGroup>
         <InputGroup className="mb-3">
-          <InputGroup.Text id="basic-addon1">會變成</InputGroup.Text>
-          <InputGroup.Text id="basic-addon1">
+          <InputGroup.Text style={{ background: "#EEEEEE" }}>
+            會變成
+          </InputGroup.Text>
+          <InputGroup.Text style={{ background: "#EEEEEE" }}>
             {Number.isFinite(newRawSnappedRatio)
               ? `1 : ${newRawSnappedRatio}`
               : ""}
@@ -75,20 +77,84 @@ function RatioTool() {
         </InputGroup>
         <hr />
         <InputGroup className="mb-3">
-          <InputGroup.Text id="basic-addon1">要變成</InputGroup.Text>
-          <InputGroup.Text id="basic-addon1">
+          <InputGroup.Text style={{ background: "#EEEEEE" }}>
+            要變成
+          </InputGroup.Text>
+          <InputGroup.Text style={{ background: "#EEEEEE" }}>
             {Number.isFinite(finalSnappedRatio)
               ? `1 : ${finalSnappedRatio}`
               : ""}
           </InputGroup.Text>
-          <InputGroup.Text id="basic-addon1">的話</InputGroup.Text>
+          <InputGroup.Text style={{ background: "#EEEEEE" }}>
+            的話
+          </InputGroup.Text>
         </InputGroup>
         <InputGroup className="mb-3" size="lg">
-          <InputGroup.Text>應該縮放</InputGroup.Text>
-          <InputGroup.Text>
+          <InputGroup.Text style={{ background: "#EEEEEE" }}>
+            應該縮放
+          </InputGroup.Text>
+          <InputGroup.Text style={{ background: "#EEEEEE" }}>
             <b>{answer || ""}</b>
           </InputGroup.Text>
-          <InputGroup.Text style={{ cursor: "pointer" }} onClick={handleCopy}>
+          <InputGroup.Text
+            style={{ cursor: "pointer", background: "#EEEEEE" }}
+            onClick={handleCopy}
+          >
+            <Scissors /> {copied ? "(已複製)" : ""}
+          </InputGroup.Text>
+        </InputGroup>
+      </Modal.Body>
+    </>
+  );
+}
+
+function RatioTool2() {
+  const [originalRatio, setOriginalRatio] = useState(1000);
+  const [newRatio, setNewRatio] = useState(200);
+  const [copied, setCopied] = useState(false);
+  const answer = useMemo(() => {
+    return _.round((originalRatio / newRatio) * 100, 4);
+  }, [originalRatio, newRatio]);
+  const handleCopy = () => {
+    navigator.clipboard.writeText(answer);
+    setCopied(true);
+  };
+
+  useEffect(() => {
+    setCopied(false);
+  }, [answer]);
+
+  return (
+    <>
+      <Modal.Body>
+        <InputGroup className="mb-3">
+          <InputGroup.Text>1 : </InputGroup.Text>
+          <Form.Control
+            value={originalRatio}
+            onChange={(v) => setOriginalRatio(v.target.value)}
+            type="number"
+          />
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <InputGroup.Text>要變成 1 : </InputGroup.Text>
+          <Form.Control
+            value={newRatio}
+            onChange={(v) => setNewRatio(v.target.value)}
+            type="number"
+          />
+        </InputGroup>
+        <hr />
+        <InputGroup className="mb-3" size="lg">
+          <InputGroup.Text style={{ background: "#EEEEEE" }}>
+            應該縮放
+          </InputGroup.Text>
+          <InputGroup.Text style={{ background: "#EEEEEE" }}>
+            <b>{answer || ""} %</b>
+          </InputGroup.Text>
+          <InputGroup.Text
+            style={{ cursor: "pointer", background: "#EEEEEE" }}
+            onClick={handleCopy}
+          >
             <Scissors /> {copied ? "(已複製)" : ""}
           </InputGroup.Text>
         </InputGroup>
@@ -103,6 +169,8 @@ function App() {
       <header className="App-header">
         <div>
           <RatioTool />
+          <hr />
+          <RatioTool2 />
         </div>
       </header>
     </div>
